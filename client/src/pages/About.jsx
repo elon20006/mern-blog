363699ed75f5a302425a7302ad4922ccc9fe000e
elon20006 <166012882+elon20006@users.noreply.github.com>
2024-04-04 20:51:34 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,28 +4,19 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About Sourav' Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, consectetur tempore. Maiores beatae dignissimos aliquam optio reprehenderit quam quidem nulla quis at ipsum!
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet distinctio aspernatur amet accusantium repellendus fuga molestiae eos unde magnam ipsa consequuntur magni dolore error, voluptatem, provident eaque sunt laborum blanditiis culpa sequi voluptatum quod accusamus. Repellat adipisci, impedit molestias ipsa reiciendis voluptates dolore.
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, sed. Dolorum est atque reiciendis asperiores? Vero similique perspiciatis quas aut aliquid iste, placeat officiis numquam omnis distinctio ipsum harum suscipit. Vel deleniti impedit voluptatem adipisci voluptatibus culpa illo suscipit, enim eveniet sit nemo voluptate libero.
             </p>
           </div>
         </div>
